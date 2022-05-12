f5c5ea83a3f5277e7a753f330f7a81b0043a14db
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-05-12 16:57:55 +0200

    deploy configs added

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: 'localhost'
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/halfblur/'
+    : '/',
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+      theme: {
+        options: {
+          customProperties: true
+        },
+        light: true,
+        themes: {
+          light: {
+            background: '#EBE5D2',
+          }
+        }
+      }
 		}
   }
 })
